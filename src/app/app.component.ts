56599612f9053e734e01ca23b5dd6b8ddbd120b0
tretyakovalex <tretyakovalexmax@gmail.com>
2025-04-30 09:57:41 +0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ConfettiService } from './services/confetti.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  habits: {name: string, isCompleted: boolean}[] = [
+    {name: "Drink Water 💧", isCompleted: false},
+    {name: "Workout 🏋️‍♂️", isCompleted: false},
+    {name: "Read 10 Pages 📚", isCompleted: false}
+  ];
+
+  constructor(private confettiService: ConfettiService){}
+
+  onCheckboxChange(event: MouseEvent, index: number): void {
+    const mouseEvent = event as MouseEvent;
+    console.log(mouseEvent);
+    this.habits[index].isCompleted = true;
+    this.confettiService.launchConfetti(mouseEvent);
+  }
 }
